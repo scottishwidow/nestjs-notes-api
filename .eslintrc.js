28447d537cc995a992a043340fb84c3fea8c5f65
Oksana Nazarchuk <oksananazarchuk667@gmail.com>
2026-02-27 20:39:34 +0100

    chore: setup husky, commitlint, linting, and CI workflows

---
+++ .eslintrc.js
@@ -0,0 +1,1 @@
+module.exports = { root: true, parser: '@typescript-eslint/parser', plugins: ['@typescript-eslint'], extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'], env: { node: true, es2021: true } };
